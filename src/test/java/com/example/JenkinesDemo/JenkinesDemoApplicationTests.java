a72ac78c874357efe8144f09b8f61df5a6d589f6
ibrahimhanna <44709558+ibrahimhanna@users.noreply.github.com>
2022-04-19 17:35:30 +0200

    Add files via upload

---
+++ src/test/java/com/example/JenkinesDemo/JenkinesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JenkinesDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
