c23573503b12c6a5749ff953742e7845d6cd69bd
ibrahimhanna <44709558+ibrahimhanna@users.noreply.github.com>
2022-04-20 10:59:24 +0200

    Update JenkinesDemoApplicationTests.java

--- src/test/java/com/example/JenkinesDemo/JenkinesDemoApplicationTests.java
+++ src/test/java/com/example/JenkinesDemo/JenkinesDemoApplicationTests.java
@@ -2,6 +2,8 @@ package com.example.JenkinesDemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.JenkinesDemo.controller.Hello;
+
 
 @SpringBootTest
 class JenkinesDemoApplicationTests {
@@ -9,5 +11,13 @@ class JenkinesDemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
+	
+          @Test
+	  void printHello() {
+	   
+	  Hello he = new Hello();
+	    assertThat(he.hello()).isEqualTo("hello welcome to java");
+	  }
 
 }
