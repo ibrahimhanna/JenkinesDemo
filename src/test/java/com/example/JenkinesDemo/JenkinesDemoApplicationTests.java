5c5edd43e7a5d5a0b6a880808b7341a0c168cade
ibrahimhanna <44709558+ibrahimhanna@users.noreply.github.com>
2022-04-20 11:05:15 +0200

    Update JenkinesDemoApplicationTests.java

--- src/test/java/com/example/JenkinesDemo/JenkinesDemoApplicationTests.java
+++ src/test/java/com/example/JenkinesDemo/JenkinesDemoApplicationTests.java
@@ -1,8 +1,11 @@
 package com.example.JenkinesDemo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.example.JenkinesDemo.controller.Hello;
+
 
 
 @SpringBootTest
